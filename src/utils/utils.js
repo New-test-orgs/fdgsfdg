2a76ca51e7afded001367f48f8032d2ab27ebfaf
volodymyr <volodymyrnoha9@gmail.com>
2022-07-13 10:38:45 +0300

    fix lint

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,10 +1,14 @@
 const go = () =>{
     console.log("dfsdsdf");
 };
- const restart = () =>{
-    console.log("restart");
+const restart = () =>{
+   console.log("restart");
+} 
+const fixLint = (fixLint) =>{
+   console.log(fixLint);
 } 
 module.exports = {
     go,
-    restart
+    restart,
+    fixLint
 }
